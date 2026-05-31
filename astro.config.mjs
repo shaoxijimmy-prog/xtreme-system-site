4aa07d9cb3b15ac391462d6973342cff04f2c2b6
shaoxijimmy-prog <shaoxijimmy@gmail.com>
2026-06-01 02:42:21 +1200

    Update site URL for lab domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://xtreme-systems.com',
+  site: 'https://lab.xtreme-systems.com',
   prefetch: true,
   build: {
     format: 'directory'
